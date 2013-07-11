d6cd550553c40ffbc1bf0edf8863323c2ef69681
Remi Arnaud <remi@acm.org>
2013-07-11 11:50:03 -0700

    first pass on upload

--- server/rest3d-basex-server.js
+++ server/rest3d-basex-server.js
@@ -48,25 +48,48 @@ var zip = require("zip");
 
 var diskcache = new cache('cache',true,false,false); 
 
-// create/delete tmp dir
+var formidable = require('formidable');
+var imageMagick = require('imageMagick');
+// Since Node 0.8, .existsSync() moved from path to fs:
+var _existsSync = fs.existsSync || path.existsSync;
+
+// create/delete tmp upload dirs
 
 var mkdirsSync = require('mkdir').mkdirsSync;
 var rimraf = require('rimraf');
 
 function delTmp(callback) {
+
+	var cb=callback;
 	rimraf('tmp/',function(err){
 		if (err)
 		{
-			console.log('Could not delete tmp'); console.log(err);
+			console.log('Could not delete tmp/'); console.log(err);
 		} else
 		{
-			console.log('tmp deleted succesfully')
+			console.log('tmp/ deleted succesfully')
 		}
 		mkdirsSync('tmp');
-		if (callback) callback();
+
+	   if (cb) callback();
 	});
+
 };
-//delTmp();
+function delUpload() {
+	rimraf('upload/',function(err){
+		if (err)
+		{
+			console.log('Could not delete upload/'); console.log(err);
+		} else
+		{
+			console.log('upload/ deleted succesfully')
+		}
+		mkdirsSync('upload');
+	});
+}
+
+delTmp();
+delUpload();
 
 var platform = os.type().match(/^Win/) ? 'win' : 
 				(os.type().match(/^Dar/) ? 'mac' : 'unix');
@@ -121,12 +144,32 @@ var session = new basex.Session(basex_port_server,basex_port);
 
 var server = restify.createServer();
 
-server.use(restify.acceptParser(server.acceptable));
+//server.use(restify.acceptParser(server.acceptable));
 //server.use(restify.authorizationParser());
-server.use(restify.dateParser());
-server.use(restify.queryParser());
-server.use(restify.bodyParser());
+//server.use(restify.dateParser());
+//server.use(restify.queryParser());
+//server.use(restify.bodyParser());
 restify.defaultResponseHeaders = false;
+
+function unknownMethodHandler(req, res) {
+	console.log('unkownMethodHandler method='+req.method.toLowerCase());
+  if (req.method.toLowerCase() === 'options') {
+    var allowHeaders = ['Accept', 'Accept-Version', 'Content-Type', 'Api-Version'];
+
+    if (res.methods.indexOf('OPTIONS') === -1) res.methods.push('OPTIONS');
+
+    res.header('Access-Control-Allow-Credentials', true);
+    res.header('Access-Control-Allow-Headers', allowHeaders.join(', '));
+    res.header('Access-Control-Allow-Methods', res.methods.join(', '));
+    res.header('Access-Control-Allow-Origin', req.headers.origin);
+
+    return res.send(204);
+  }
+  else
+    return res.send(new restify.MethodNotAllowedError());
+}
+
+server.on('MethodNotAllowed', unknownMethodHandler);
 /*
 server.use(restify.throttle({
   burst: 100,
@@ -201,12 +244,13 @@ function print(err, reply) {
 };
 
 basexGet= function(url, callback) {
+	var cb=callback;
 	basex_client.get(url, function(err, req2) {
 		console.log('calling BASEX REST GET = '+ url)
 		if (err){
 			console.log('BASEX REST GET ERROR')
 			console.log(err)
-			if (callback) callback(err,req2,null);
+			if (cb) cb(err,req2,null);
 			else return err;
 		}
 
@@ -214,7 +258,7 @@ basexGet= function(url, callback) {
 	  	if (err){
 			console.log('BASEX REST GET RESULT ERROR')
 			console.log(err)
-			if (callback) callback(err,req2,null);
+			if (cb) cb(err,req2,null);
 			else return err;
 		}
 		res2.body=''
@@ -224,7 +268,7 @@ basexGet= function(url, callback) {
 	    });
 
 	    res2.on('end', function() {
-	      if (callback) callback(null, req2, res2);
+	      if (cb) cb(null, req2, res2);
 	      else return res2;
 	    });
 	  });
@@ -232,6 +276,7 @@ basexGet= function(url, callback) {
 	});
 };
 basexPost= function(url, body, callback) {
+	var cb=callback;
 	var opts = {};
 	opts['path'] = '/rest'+url;
 	opts['headers'] = {'content-type': 'application/xml'};
@@ -241,7 +286,7 @@ basexPost= function(url, body, callback) {
 		if (err){
 			console.log('BASEX REST GET ERROR')
 			console.log(err)
-			if (callback) callback(err,req2,null);
+			if (cb) cb(err,req2,null);
 			else return err;
 		}
 	  req2.write(body);
@@ -251,7 +296,7 @@ basexPost= function(url, body, callback) {
 	  	if (err){
 			console.log('BASEX REST GET RESULT ERROR')
 			console.log(err)
-			if (callback) callback(err,req2,null);
+			if (cb) cb(err,req2,null);
 			else return err;
 		}
 		res2.body=''
@@ -261,7 +306,7 @@ basexPost= function(url, body, callback) {
 	    });
 
 	    res2.on('end', function() {
-	      if (callback) callback(null, req2, res2);
+	      if (cb) cb(null, req2, res2);
 	      else return res2;
 	    });
 	  });
@@ -291,7 +336,7 @@ session.execute("xquery json:serialize-ml(db:system())", function (err,r){
 	}
 });
 
-function mySend(p,req,res) {
+function sendFile(req,res,p) {
   function error(err) {
     res.statusCode = err.status;
     res.end(http.STATUS_CODES[err.status]);
@@ -308,7 +353,7 @@ function mySend(p,req,res) {
     console.log('redirected to '+req.url + 'index.html')
   }
 
-console.log('mySend dir='+ p);
+console.log('sendFile dir='+ p);
 
   send(req, p)
   .on('error', error)
@@ -816,7 +861,7 @@ server.get(/^\/rest3d\/assets.*/,function(req, res, next) {
 	    		console.log('disk cache HIT!');
 	    		res.setHeader('Content-Type', entry.headers['content-type']);
 	    		console.log('set content-type to ['+entry.headers['content-type']+']')
-				mySend(entry.filename,req,res);
+				sendFile(req,res,entry.filename);
 				return next();
 	    	} // else
 
@@ -868,7 +913,7 @@ server.get(/^\/rest3d\/assets.*/,function(req, res, next) {
 				    				console.log('SERVE CONTENT NOW!!='+entry.filename)
 				    				res.setHeader('Content-Type', entry.headers['content-type']);
 						    		console.log('set content-type to ['+entry.headers['content-type']+']')
-									mySend(entry.filename,req,res);
+									sendFile(req,res,entry.filename);
 									return next();
 								});
 		    			});
@@ -910,7 +955,7 @@ server.get(/^\/rest3d\/assets.*/,function(req, res, next) {
 				    				console.log('SERVE CONTENT NOW!!='+entry.filename)
 				    				res.setHeader('Content-Type', entry.headers['content-type']);
 						    		console.log('set content-type to ['+entry.headers['content-type']+']')
-									mySend(entry.filename,req,res);
+									sendFile(req,res,entry.filename);
 									return next();
 								});
 
@@ -941,7 +986,289 @@ server.get(/^\/rest3d\/assets.*/,function(req, res, next) {
 	
 });
 
+// upload
+var UploadHandler = function (req, res, callback) {
+            this.req = req;
+            this.res = res;
+            this.callback = callback;
+    },
+    options = {
+        tmpDir: __dirname + '/tmp',
+        uploadDir: __dirname + '/upload',
+        uploadUrl: '/files/',
+        maxPostSize: 11000000000, // 11 GB
+        minFileSize: 1,
+        maxFileSize: 10000000000, // 10 GB
+        acceptFileTypes: /.+/i,
+        // Files not matched by this regular expression force a download dialog,
+        // to prevent executing any scripts in the context of the service domain:
+        safeFileTypes: /\.(gif|jpe?g|png|tga|dae|zip)$/i,
+        imageTypes: /\.(gif|jpe?g|png|tga)$/i,
+        imageVersions: {
+            'thumbnail': {
+                width: 80,
+                height: 80
+               }
+       },
+       accessControl: {
+            allowOrigin: '*',
+            allowMethods: 'OPTIONS, HEAD, GET, POST, PUT, DELETE',
+            allowHeaders: 'Content-Type, Content-Range, Content-Disposition'
+        },
+        /* Uncomment and edit this section to provide the service via HTTPS:
+        ssl: {
+            key: fs.readFileSync('/Applications/XAMPP/etc/ssl.key/server.key'),
+            cert: fs.readFileSync('/Applications/XAMPP/etc/ssl.crt/server.crt')
+        },
+        */
+       nodeStatic: {
+           cache: 3600 // seconds to cache served files
+       }
+    },
+    utf8encode = function (str) {
+        return unescape(encodeURIComponent(str));
+    },
 
+    nameCountRegexp = /(?:(?: \(([\d]+)\))?(\.[^.]+))?$/,
+    nameCountFunc = function (s, index, ext) {
+        return ' (' + ((parseInt(index, 10) || 0) + 1) + ')' + (ext || '');
+    },
+    handleResult = function (req, res, result, redirect) {
+
+        if (redirect) {
+            res.writeHead(302, {
+                'Location': redirect.replace(
+                /%s/,
+                encodeURIComponent(JSON.stringify(result))
+                )
+            });
+            res.end();
+        } else {
+            res.writeHead(200, {
+                'Content-Type': req.headers.accept
+                .indexOf('application/json') !== -1 ?
+                    'application/json' : 'text/plain'
+            });
+            res.end(JSON.stringify(result));
+        }
+    },
+    setNoCacheHeaders = function (res) {
+        res.setHeader('Pragma', 'no-cache');
+        res.setHeader('Cache-Control', 'no-store, no-cache, must-revalidate');
+        res.setHeader('Content-Disposition', 'inline; filename="files.json"');
+    };
+
+
+var FileInfo = function (file) {
+        this.name = file.name;
+        this.size = file.size;
+        this.type = file.type;
+        this.delete_type = 'DELETE';
+};
+FileInfo.prototype.validate = function () {
+    if (options.minFileSize && options.minFileSize > this.size) {
+        this.error = 'File is too small';
+    } else if (options.maxFileSize && options.maxFileSize < this.size) {
+        this.error = 'File is too big';
+    } else if (!options.acceptFileTypes.test(this.name)) {
+        this.error = 'Filetype not allowed';
+    }
+    return !this.error;
+};
+FileInfo.prototype.safeName = function () {
+    // Prevent directory traversal and creating hidden system files:
+    this.name = path.basename(this.name).replace(/^\.+/, '');
+    // Prevent overwriting existing files:
+    while (_existsSync(options.uploadDir + '/' + this.name)) {
+        this.name = this.name.replace(nameCountRegexp, nameCountFunc);
+    }
+};
+FileInfo.prototype.initUrls = function (req) {
+    if (!this.error) {
+        var that = this,
+            baseUrl = (options.ssl ? 'https:' : 'http:') +
+                '//' + req.headers.host + options.uploadUrl;
+        this.url = this.delete_url = baseUrl + encodeURIComponent(this.name);
+        Object.keys(options.imageVersions).forEach(function (version) {
+            if (_existsSync(
+                    options.uploadDir + '/' + version + '/' + that.name
+                )) {
+                that[version + '_url'] = baseUrl + version + '/' +
+                    encodeURIComponent(that.name);
+            }
+        });
+    }
+};
+
+var UploadHandler = function (req, res, callback) {
+    this.req = req;
+    this.res = res;
+    this.callback = callback;
+};
+
+
+UploadHandler.prototype.post = function () {
+    var handler = this,
+        form = new formidable.IncomingForm(),
+        tmpFiles = [],
+        files = [],
+        map = {},
+        counter = 1,
+        redirect,
+        finish = function () {
+            counter -= 1;
+            if (!counter) {
+                files.forEach(function (fileInfo) {
+                    fileInfo.initUrls(handler.req);
+                });
+                handler.callback(handler.req, handler.res, {files: files}, redirect);
+            }
+        };
+    form.uploadDir = options.tmpDir;
+    form.on('fileBegin', function (name, file) {
+        tmpFiles.push(file.path);
+        var fileInfo = new FileInfo(file, handler.req, true);
+        fileInfo.safeName();
+        map[path.basename(file.path)] = fileInfo;
+        files.push(fileInfo);
+    }).on('field', function (name, value) {
+        if (name === 'redirect') {
+            redirect = value;
+        }
+    }).on('file', function (name, file) {
+        var fileInfo = map[path.basename(file.path)];
+        fileInfo.size = file.size;
+        if (!fileInfo.validate()) {
+            fs.unlink(file.path);
+            return;
+        }
+        fs.renameSync(file.path, options.uploadDir + '/' + fileInfo.name);
+        if (options.imageTypes.test(fileInfo.name)) {
+            Object.keys(options.imageVersions).forEach(function (version) {
+                counter += 1;
+                var opts = options.imageVersions[version];
+                imageMagick.resize({
+                    width: opts.width,
+                    height: opts.height,
+                    srcPath: options.uploadDir + '/' + fileInfo.name,
+                    dstPath: options.uploadDir + '/' + version + '/' +
+                        fileInfo.name
+                }, finish);
+            });
+        }
+    }).on('aborted', function () {
+        tmpFiles.forEach(function (file) {
+            fs.unlink(file);
+        });
+    }).on('error', function (e) {
+        console.log(e);
+        return ('error '+e)
+    }).on('progress', function (bytesReceived, bytesExpected) {
+        if (bytesReceived > options.maxPostSize) {
+            handler.req.connection.destroy();
+        }
+    }).on('end', finish);
+
+    form.parse(handler.req);
+    return 'files upload succesfully';
+};
+
+UploadHandler.prototype.get = function () {
+    var handler = this,
+        files = [];
+    fs.readdir(options.uploadDir, function (err, list) {
+        list.forEach(function (name) {
+            var stats = fs.statSync(options.uploadDir + '/' + name),
+                fileInfo;
+            if (stats.isFile() && name[0] !== '.') {
+                fileInfo = new FileInfo({
+                    name: name,
+                    size: stats.size
+                });
+                fileInfo.initUrls(handler.req);
+                files.push(fileInfo);
+            }
+        });
+        handler.callback(handler.req, handler.res, {files: files});
+    });
+};
+
+UploadHandler.prototype.destroy = function () {
+    var handler = this,
+        fileName;
+    if (handler.req.url.slice(0, options.uploadUrl.length) === options.uploadUrl) {
+        fileName = path.basename(decodeURIComponent(handler.req.url));
+        if (fileName[0] !== '.') {
+            fs.unlink(options.uploadDir + '/' + fileName, function (ex) {
+                Object.keys(options.imageVersions).forEach(function (version) {
+                    fs.unlink(options.uploadDir + '/' + version + '/' + fileName);
+                });
+                handler.callback(this.req, this.res, {success: !ex});
+            });
+            return;
+        }
+    }
+    handler.callback(this.req, this.res, {success: false});
+};
+
+server.post(/^\/rest3d\/upload.*/, function(req,res,next){
+
+	console.log('in post upload');
+    res.setHeader(
+        'Access-Control-Allow-Origin',
+        options.accessControl.allowOrigin
+    );
+    res.setHeader(
+        'Access-Control-Allow-Methods',
+        options.accessControl.allowMethods
+    );
+    res.setHeader(
+        'Access-Control-Allow-Headers',
+        options.accessControl.allowHeaders
+    );
+    var handler = new UploadHandler(req, res, handleResult);
+    setNoCacheHeaders(res);
+    var result = handler.post();
+    // say something ?
+    /*
+    res.writeHead(200, {'Content-Type': 'application/json' });
+	res.write(toJSON(result));
+	res.end();
+*/
+    return next();
+});
+
+server.get(/^\/rest3d\/upload.*/, function(req,res,next){
+	console.log('in upload/')
+    res.setHeader(
+        'Access-Control-Allow-Origin',
+        options.accessControl.allowOrigin
+    );
+    res.setHeader(
+        'Access-Control-Allow-Methods',
+        options.accessControl.allowMethods
+    );
+    res.setHeader(
+        'Access-Control-Allow-Headers',
+        options.accessControl.allowHeaders
+    );
+    var handler = new UploadHandler(req, res, handleResult);
+
+    var asset = req.url.split("/upload/")[1];
+    console.log('asset='+asset)
+    if (asset === undefined || asset === '') {
+        setNoCacheHeaders(res);
+        if (req.method === 'GET') {
+            handler.get();
+        } else {
+           res.end();
+        }
+     } else {
+     	 var p=path.resolve(options.uploadDir+'/'+asset);
+         sendFile(req,res,p);
+     }
+     return next();
+});
 // static server
 server.get(/^\/.*/, function (req, res, next) {
 	
@@ -950,7 +1277,7 @@ server.get(/^\/.*/, function (req, res, next) {
 
 	console.log('http get path='+p);
 
-	mySend(p,req,res);
+	sendFile(req,res,p);
 	return next();
 });
 
