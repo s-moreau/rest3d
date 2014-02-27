d01688d3f68af185b9faf1e5c9c2888abb562d44
Remi Arnaud <remi@acm.org>
2014-02-26 23:44:58 -0800

    updated to tot gltf format

--- static/models/cow/cow0FS.glsl
+++ static/models/cow/cow0FS.glsl
@@ -6,9 +6,9 @@ void main(void) {
 vec3 normal = normalize(v_normal);
 vec4 color = vec4(0., 0., 0., 0.);
 vec4 diffuse = vec4(0., 0., 0., 1.);
-vec3 diffuseLight = vec3(0., 0., 0.);
 diffuse = texture2D(u_diffuse, v_texcoord0);
 diffuse.xyz *= max(dot(normal,vec3(0.,0.,1.)), 0.);
 color.xyz += diffuse.xyz;
-gl_FragColor = vec4(color.rgb * diffuse.a, diffuse.a);
+color = vec4(color.rgb * diffuse.a, diffuse.a);
+gl_FragColor = color;
 }
