1084ee56c95e0e15f76502fd399aa44992792f0d
MaximeHelen <m9helen@enib.fr>
2014-03-03 20:57:24 -0800

    Improved upload widget

--- static/deps/upload/viewer6-upload.js
+++ static/deps/upload/viewer6-upload.js
@@ -116,7 +116,7 @@ setViewer6Upload=function(upload){
         upload.object=$(this);
         data.context = header;
         $.each(data.files, function (index, file) {
-            if (!index) {
+            // if (!index) {
                 var $dialog = $("<button>Launch your model</button>").on("click",function(){
                                 glTF.load(url, viewer.parse_gltf);
                                 window.notif(url);
@@ -135,7 +135,7 @@ setViewer6Upload=function(upload){
                             });
                 upload.upload(header,file.name,$dialog,$preview,uploadButton.clone(true).data(data));
             }
-            else{this.upload(header,file.name);}
+            // else{this.upload(header,file.name);}
         });
     }).on('fileuploadprocessalways', function (e, data) {
             var indexI = data.index,
@@ -154,7 +154,6 @@ setViewer6Upload=function(upload){
         }
         if (indexI + 1 === data.files.length) {
             node.find('button')
-                .text('Upload')
                 .prop('disabled', !!data.files.error);
         }
         index++;
