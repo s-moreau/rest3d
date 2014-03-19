6d737179f439066dc2d6918884f535a2cbff1b2b
MaximeHelen <m9helen@enib.fr>
2014-03-19 11:57:46 -0700

    TOuch

--- static/viewer/viewer6.js
+++ static/viewer/viewer6.js
@@ -21,7 +21,7 @@ AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
 LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 THE SOFTWARE.*/
-define(['jquerymin','gui','console', 'gltf', 'collada', 'renderer','camera','glmatrixExt','rest3d','uploadViewer'], function ($,GUI,CONSOLE,glTF,COLLADA,rest3d) {
+define(['jquerymin','gui','console', 'gltf', 'collada', 'renderer','camera','glmatrixExt','rest3d','uploadViewer'], function ($,GUI,CONSOLE,glTF,COLLADA,rest3d,setViewer6Upload) {
 
 var viewer  = {};
 var scenes = [];
