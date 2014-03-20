606cd02da9f44ff1b38189ad9dacbd91b26132d4
MaximeHelen <m9helen@enib.fr>
2014-03-20 15:24:35 -0700

    Touch

--- static/viewer/viewer6.js
+++ static/viewer/viewer6.js
@@ -1269,19 +1269,19 @@ if (!window.performance || !window.performance.now) {
             href[0].remove();
         }
         function display(node){
-            var uri = node.attr("asseturi");
-            var http = new XMLHttpRequest();
-            http.open("POST", "/rest3d/upload/"+uri, true);
-            http.setRequestHeader("Content-type","application/x-www-form-urlencoded");
-            var params = {};
-            params.id ="url";
-            params.size ="150";
-            params.value = "/rest3d/upload/"+uri;
-            params.url = "/rest3d/upload/"+uri;
-            http.send(params);
-            http.onload = function() {
-                alert(http.responseText);
-            }
+            // var uri = node.attr("asseturi");
+            // var http = new XMLHttpRequest();
+            // http.open("POST", "/rest3d/upload/"+uri, true);
+            // http.setRequestHeader("Content-type","application/x-www-form-urlencoded");
+            // var params = {};
+            // params.id ="url";
+            // params.size ="150";
+            // params.value = "/rest3d/upload/"+uri;
+            // params.url = "/rest3d/upload/"+uri;
+            // http.send(params);
+            // http.onload = function() {
+            //     alert(http.responseText);
+            // }
             // $.post("/rest3d/upload/"+uri, function( data ) {
             //     console.debug("data");
             // }).fail(function() {
@@ -1298,13 +1298,9 @@ if (!window.performance || !window.performance.now) {
       
             //          });
             // console.error("REMIIIIII affiche moi le model: "+node.attr("id") +" :-)")
-            // var $html = '<form id="url-upload" action="/rest3d/upload/" enctype="application/x-www-form-urlencoded" method="POST" ><input id="url" size="150" name="url" value="'+uri+'" style="display:none !important;"/><input id="acceptSubmit" type="submit" value="Upload" style="display:none !important;"/></form>';
-            // $('body').append($html);
-            // var data;
-            // $html.submit(function(){
-            //     console.debug("submit");
-            // })
-            // $('#acceptSubmit').click()
+            var $html = '<form id="url-upload" action="/rest3d/upload/" enctype="application/x-www-form-urlencoded" method="POST" ><input id="url" size="150" name="url" value="'+uri+'" style="display:none !important;"/><input id="acceptSubmit" type="submit" value="Upload" style="display:none !important;"/></form>';
+            $('body').append($html);
+            $('#acceptSubmit').click();
         }
         function preview(node){
             $("#dialog").dialog("close");
