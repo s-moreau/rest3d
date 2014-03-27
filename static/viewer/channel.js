b56f77d73ce9b84bd8b65d092874e4ce615eba23
Remi Arnaud <remi@acm.org>
2014-03-26 17:33:25 -0700

    fixed easyviewer require, added background color parameter

--- static/viewer/channel.js
+++ static/viewer/channel.js
@@ -22,7 +22,7 @@ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 THE SOFTWARE.*/
 /// 'use strict'; problem witd defining function inside test
-define(['state'], function (State) {
+define(['WebGLDebugUtils','state'], function (WebGLDebugUtils,State) {
   var Channel=  {};
 
   // active channel
