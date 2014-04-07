07148e6c68948260e2b8eeb7e06bfab5ff9ffc4f
MaximeHelen <m9helen@enib.fr>
2014-04-07 16:27:01 -0700

    Touch

--- static/viewer/viewer6-upload.js
+++ static/viewer/viewer6-upload.js
@@ -148,14 +148,14 @@ define(['jquerymin', 'rest3d', 'gltf', 'collada', 'viewer','q'], function ($, re
                             // ennumerate all resulting files
                             var $conve = upload.header(data.file.name);
                             $.each(data.result.files, function (index, file) {
-                                var href = $('<a style="display:none" href="/rest3d/upload/' + decodeURIComponent(file.name) + '" target="_blank"></a>');
+                                var href = $('<a style="display:none" href="/rest3d/' + decodeURIComponent(file.name) + '" target="_blank"></a>');
                                 upload.filesArea.append(href);
                                 var $download = $("<button>Download</button>").on("click", function () {
                                     href[0].click();
                                     href[0].remove();
                                 });
 
-                                var url = '/rest3d/upload/' + decodeURIComponent(file.name);
+                                var url = '/rest3d/' + decodeURIComponent(file.name);
                                 var ext = url.match(/\.[^.]+$/);
                                 if (ext == ".json") {
                                     var $dialog = $("<button>Launch</button>").on("click", function () {
